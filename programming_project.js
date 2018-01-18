2b2217bbf7764adc31ec688bbce2b731e4c8d5c8
DaphneBox2 <daphne.box@student.uva.nl>
2018-01-18 21:35:07 +0100

    update 18-01

--- programming_project.js
+++ programming_project.js
@@ -4,24 +4,13 @@ this javascript file is the main file for my programming project where all impor
 */
 
 // main function
-window.onload = function main(){
+window.onload = function main() {
 	// load data
-	d3.csv("basicIncomeDoubleCountries.csv", function(data){
+	d3.csv("basicIncomeDoubleCountries.csv", function( data ) {
 		
-		// console.log(data);
-		// select question, to start with only data from question vote for will be selected!!!
-		// var question = [];
-		// var countries = [];
-		// data.forEach(function(d){
-		// 	console.log(d.country_code_3);
-		// 	question.push(d.basic_income_vote, d.country_code_3);
-		// 	countries.push(d.country_code_3);
-		// })
-		// console.log(question);
-		// console.log(countries);
-		drawMap(data);
-		drawBarGraph(data);
-		// colorMap(data);
+		drawMap( data );
+		drawBarGraph( data );
+		drawParallelCoordinatesGraph( data );
 	})
 }
 
@@ -31,100 +20,99 @@ window.onload = function main(){
 code comes from: http://bl.ocks.org/milafrerichs/69035da4707ea51886eb
 json of Europe comes from: https://geojson-maps.ash.ms/
 */
-function drawMap(dataColors){
-	(function(){
-		var center, height, path, projection, scale, svg, width;
-		width = 700;
-		height = 1000;
-		center = [5, 70];
-		scale = 500;
-		projection = d3.geo
+function drawMap( dataColors ) {
+	( function() {
+		var width = 700;
+		var height = 1000;
+		var center = [5, 70];
+		var scale = 500;
+		var projection = d3.geo
 			.mercator()
-			.scale(scale)
-			.translate([width / 2, 0])
-			.center(center);
-		path = d3.geo
+			.scale( scale )
+			.translate( [width / 2, 0] )
+			.center( center );
+		var path = d3.geo
 			.path()
-			.projection(projection);
-		column = d3.select(".map");
+			.projection( projection );
+		var column = d3.select( ".map" );
 				
-		svg = column.select("svg")		
-			.attr("height", height)
-			.attr("width", width);
+		var svg = column.select( "svg" )		
+			.attr( "height", height )
+			.attr( "width", width );
 		
-		d3.json("scripts/custom-geo-3_topojson.json", function(error, data) {
-			if (error){
+		d3.json("scripts/custom-geo-3_topojson.json", function( error, data ) {
+
+			if ( error ){
 				throw error;
 			}
 
-			var europeMap = topojson.feature(data, data.objects.customgeo3);
-			console.log(europeMap);
+			var europeMap = topojson.feature( data, data.objects.customgeo3 );
 
-			svg.selectAll(".europeMap")
-					.data(topojson.feature(data, data.objects.customgeo3).features)
+			svg.selectAll( ".europeMap" )
+					.data( topojson.feature( data, data.objects.customgeo3 ).features )
 				.enter()
-				.append("path")
-					.attr("id", function(d) {return d.properties.sov_a3;})
-					.attr("stroke", "#000000")
-					.attr("fill", "#E0E0E0")
-					.attr('d', path);
+				.append( "path" )
+					.attr( "id", function( d ) { return d.properties.sov_a3; } )
+					.attr( "stroke", "#000000" )
+					.attr( "fill", "#E0E0E0" )
+					.attr( 'd', path );
 
-			colorMap(dataColors);
+			colorMap( dataColors );
 		});
 
 		
-	}).call(this);
+	}).call( this );
 }
 
-// colorMap function
-function colorMap(data){
-	console.log(data);
+/* 
+colorMap function
+gives the countries on the map a color according to corresponding data
+*/
 
-
+function colorMap( data ) {
+	
 	// declare necessary variables, choice variables are declared in order of description of codebook_basicIncome.pdf file
 	var countryList = [];
 
-	for( var i = 1; i < data.length; i++ ){
+	for ( var i = 1; i < data.length; i++ ) {
 
-		if ( i < (data.length - 1 ) ){
+		if ( i < ( data.length - 1 ) ) {
 
 			// determine how many countries are surveyed
-			if ( data[ i - 1 ].country_code_3 != data[ i ].country_code_3 ){
-				countryList.push( data[i - 1].country_code_3 );
+			if ( data[i - 1].countryCode3 != data[i].countryCode3 ) {
+				countryList.push( data[i - 1].countryCode3 );
 			}
 		}
-		else{
+		else {
 
-			countryList.push( data[i - 1].country_code_3 );
+			countryList.push( data[i - 1].countryCode3 );
 		}
-		console.log(countryList);
 	}
 
 	// loop through the list of all surveyed countries
-	for( var j = 0; j < countryList.length; j++){
+	for ( var j = 0; j < countryList.length; j++ ) {
 
 		country = countryList[j];
 		
 		// determine which options is chosen most often and determine color
-		var returncalculateChoices = calculateChoice(data, country);
-		var choicesList = [ "choice1", "choice2", "choice3", "choice4", "choice5", "other" ];
+		var returncalculateChoices = calculateChoice( data, country );
+		var choicesList = ["choice1", "choice2", "choice3", "choice4", "choice5", "other"];
 		var choices = [];
-		var colorCountries = [ "#B71C1C", "#E57373", "#64B5F6", "#0D47A1", "#616161" ];
+		var colorCountries = ["#B71C1C", "#E57373", "#64B5F6", "#0D47A1", "#616161"];
 		var choiceMax = 0;
 		var choiceVar = "";
 		var choiceColor;
 		var mostChosenPercentage;
 
 		// put necessary data in choices
-		for ( var l = 0; l < (returncalculateChoices.length - 1); l++){
+		for ( var l = 0; l < ( returncalculateChoices.length - 1 ); l++ ) {
 
 			choices.push( returncalculateChoices[l] );
 		}
-		console.log(returncalculateChoices);
-		console.log(choices);
-		for( var m = 0; m < choices.length; m++ ){
 
-			if( choiceMax < choices[m]){
+		for ( var m = 0; m < choices.length; m++ ) {
+
+			if ( choiceMax < choices[m] ) {
 
 				choiceMax = choices[m];
 				choiceVar = choicesList[m];
@@ -133,35 +121,39 @@ function colorMap(data){
 		}
 		
 		mostChosenPercentage = ( choiceMax / returncalculateChoices[( returncalculateChoices.length - 1 )] ) * 100;
-		console.log("country:", countryList[ j ], "most chosen:", choiceVar, "with percentage:", mostChosenPercentage, choiceColor);
 
 		// color country
-		var currentCountry = "#" + countryList[ j ] + "";
+		var currentCountry = "#" + countryList[j] + "";
 
-		d3.selectAll(currentCountry)
-			.attr("fill", choiceColor);
+		d3.selectAll( currentCountry )
+			.attr( "fill", choiceColor );
 	}
 }
 
 // updateMap function
 
-// drawBarGraph function
-function drawBarGraph(data){
+/*
+drawBarGraph function
+this function draws for the first time the bar graph of the website after country click. 
+Code inspired from: https://bost.ocks.org/mike/bar/
+*/
+
+function drawBarGraph( data ) {
 
 	// make data complete for a country
 	var countryCode = "DEU";
-	var dataCountry = calculateChoice(data, countryCode);
-	var choicesList = [ "choice1", "choice2", "choice3", "choice4", "choice5", "other" ];
+	var dataCountry = calculateChoice( data, countryCode );
+	var choicesList = ["choice1", "choice2", "choice3", "choice4", "choice5", "other"];
 
 	// calculate percentages
 	var dataCountryPercentages = [];
 
-	for ( var i = 0; i < (dataCountry.length - 1 ); i++ ){
+	for ( var i = 0; i < ( dataCountry.length - 1 ); i++ ) {
 
 		var percentage = ( dataCountry[i] / dataCountry[dataCountry.length - 1] ) * 100;
 		dataCountryPercentages.push( { "choice": choicesList[i], "percentage" : percentage } );
 	}
-	console.log(dataCountryPercentages[0].percentage);
+	
 	// state dimensions
 	var margin = { top: 100, right: 30, bottom: 30, left: 30 };
 	var width = 400 - margin.left - margin.right;
@@ -171,55 +163,170 @@ function drawBarGraph(data){
 		.range( [height, 0] );
 
 	var x = d3.scale.ordinal()
-		.rangeRoundBands([0, width], .1);
+		.rangeRoundBands( [0, width], .1 );
 
-	function(error, dataCountryPercentages){
+	// make translation formula to calculate the bar heigth and width
+	x.domain( dataCountryPercentages.map( function ( d ) { return d.choice; } ) );
+	y.domain( [0, d3.max( dataCountryPercentages, function ( d ) { return d.percentage; } )] );
 
-		// check for errors
-		if (error) throw error;
+	var chart = d3.select( ".barChart" )
+		.attr( "width", width )
+		.attr( "height", height );
 
-		// make translation formula to calculate the bar heigth and width
-		x.domain( dataCountryPercentages.map( function (d) { return d.choice; }));
-		y.domain( [ 0, d3.max( dataCountryPercentages, function (d) { return d.percentage; })]);
+	var barWidth = width / dataCountryPercentages.length;
 
-		var chart = d3.select(".barChart")
-			.attr( "width", width )
-			.attr( "height", height );
+	var bar = chart.selectAll( "g" )
+			.data( dataCountryPercentages )
+		.enter()
+		.append( "g" )
+			.attr( "transform", function( d ){ return "translate( " + ( margin.left + x( d.choice )) + ",0 )"; } );
 
-		var barWidth = width / data.length;
+	bar.append( "rect" )
+		.attr( "x", function( d ) { return x( d.choice ); } )
+		.attr( "y", function( d ) { return y( d.percentage ); } )
+		.attr( "height", function( d ) { ( height - y( d.percentage ) ); } )
+		.attr( "width", barWidth - 1 )
+		.attr( "fill", "blue" );
 
-		var bar = chart.selectAll("g")
-				.data(data)
-			.enter()
-			.append( "g" )
-				.attr( "transform", function( d, i ){ return "translate(" + i * barWidth + ",0)"; } );
+	bar.append( "text" )
+		.attr( "x", barWidth / 2 )
+		.attr( "y", function( d ) { return y( d.percentage ); } )
+		.attr( "dy", ".75em" )
+		.text( function( d ) { return d.choicesList; } );
 
-		bar.append("rect")
-			.attr( "x", function(d) { return x ( d.choice );})
-			.attr( "y", function(d) { return y ( d.percentage );})
-			.attr( "height", function(d){ height - y( d.percentage );})
-			.attr("width", barWidth - 1 );
+	// make axis
+	var xAxis = d3.svg.axis()
+		.scale( x )
+		.orient( "bottom" );
 
-		bar.append("text")
-			.attr("x", barWidth / 2 )
-			.attr("y", function(d){ return y(d.percentage); })
-			.attr("dy", ".75em")
-			.text(function(d){ return d.choicesList ;});
+	chart.append( "g" )
+		.attr( "class", "x axis" )
+		.attr( "transform", "translate( 30," + height + " )" )
+		.call( xAxis );	
+	
+	chart.append( "text" )
+		.attr( "class", "label" )
+		.attr( "x", width )
+		.attr( "y", height + 30 )
+		.style( "text-anchor", "end" )
+		.text( "choice" );
 
-		// make axis
-		var xAxis = chart.svg.axis()
-			.scale(y)
-			.orient("bottom");
+	var yAxis = d3.svg.axis()
+		.scale( y )
+		.orient( "left" );
 
-		var yAxis = chart.svg.axis()
-			.scale(x)
-			.orient("left");
-	}
+	chart.append( "g" )
+		.attr( "class", "y axis" )
+		.attr( "transform", "translate( 30," + height + " )" )
+		.call( yAxis );	
+	
+	chart.append( "text" )
+		.attr( "class", "label" )
+		.attr( "x", 100 )
+		.attr( "y", 10 )
+		.style( "text-anchor", "end" )
+		.text( "percentage" );
 }
 
 // updateBarGraph function
 
-// drawParallelCoordinatesGraph function
+/*
+ drawParallelCoordinatesGraph function
+code for parallel coordinates graph is derived from: https://bl.ocks.org/mbostock/1341021
+*/
+
+function drawParallelCoordinatesGraph( data ) {
+
+	// state dimentions
+	var margin = { top: 100, right: 30, bottom: 30, left: 30 };
+	var width = 400 - margin.left - margin.right;
+	var height = 500 - margin.top - margin.bottom;
+
+	// declare other variables
+	var x = d3.scale.ordinal().rangePoints( [0, width], 1 );
+	var y = {};
+	
+	var line = d3.svg.line();
+	var axis = d3.svg.axis()
+		.orient( "left" );
+	
+	var background;
+	var foreground;
+
+	var chart = d3.select( ".parallelOrientations" )
+			.attr( "width", width + margin.left + margin.right )
+			.attr( "heigth", height + margin.top + margin.bottom )
+		.append( "g" )
+			.attr( "transform", "translate( " + margin.left + "," + margin.top + " )" );
+
+	// extract list of dimensions and create a scale for each
+	x.domain( dimensions = d3.keys( data[0] )
+		.filter( function( d ) { return d != "countryCode2" && d != "countryCode3" && d != "age" 
+			&& d != "gender" && d != "ruralUrban" && d != "basicIncomeAwareness" && d != "basicIncomeEffect" 
+			&& d != "basicIncomeArgumentsFor" && d != "basicIncomeArgumentsAgainst" && d != "weight" 
+			&& ( y[d] = d3.scale.linear()
+			.domain( d3.extent( data, function( p ) { return + p[d]; } ) )
+			.range( [height, 0] ) )
+	} ) );
+	
+	// add grey background lines for context
+	background = chart.append( "g" )
+    	.attr( "class", "background" )
+    .selectAll( "path" )
+    	.data( data )
+    .enter()
+    .append( "path" )
+    	.attr( "d", path );
+
+	// add blue foreground lines for focus
+	foreground = svg.append( "g" )
+    	.attr( "class", "foreground" )
+    .selectAll( "path" )
+    	.data( data )
+    .enter()
+    .append( "path" )
+    	.attr( "d", path );
+
+  // add a group element for each dimension
+  var g = chart.selectAll( ".dimension" )
+  		.data( dimensions )
+	.enter()
+	.append( "g" )
+		.attr( "class", "dimension" );
+		// .attr( "transform", function( d ) { retrurn "translate( " + x(d) + " )"; } );
+
+	// add an axis and title
+	g.append( "g" )
+			.attr( "class", "axis" )
+			.each( function( d ) { d3.select( this ).call( axis.scale( y )[d] ); } ) 
+		.append( "text" )
+			.style( "text-anchor", "middle" )
+			.attr( "y", -9 )
+			.text()
+
+	// add and store a brush for each axis
+	g.append( "g" )
+			.attr( "class", "brush" )
+			.each( function( d ) { d3.select( this ).call( y[d].brush = d3.svg.brush().y( y[d] ).on( "brush", brush ) ); } ) 
+		selectAll( "rect" )
+			.attr( "x", -8 )
+			.attr( "width", 16 );
+
+	// returns path for given data point
+	function path ( d ) {
+
+		return line( dimensions.map( function ( p ) { return [x( p ), y[p]( d[p] )]; } ) );
+	}
+
+	// handles a brush event, toggling the display of foreground lines
+	function brush() {
+
+		var actives = dimensions.filter( function ( p ) { return !y[p].brush.empty(); } );
+		var extents = actives.map( function ( p ) { return y[p].brush.extent(); } );
+
+		foreground.style("display", function( d ) { return actives.every( function ( p, i ) { return extents[i][0] <= d[p] && d[p] <= extents[i][1]; } ) ? null : "none"; } )
+	}
+}
 
 // updateParallelCoordinatesGraph function
 
@@ -227,7 +334,7 @@ function drawBarGraph(data){
 calculateChoice function
 function calculates the total times a choice option is chosen by participants from a certain country 
 */
-function calculateChoice(data, country){
+function calculateChoice( data, country ) {
 
 	// declare variables necessary to count votes and participants
 	var totalParticipants = 0;
@@ -239,56 +346,58 @@ function calculateChoice(data, country){
 	var other = 0;
 
 	// loop through dataset
-	for( var k = 0; k < data.length; k++){
+	for ( var k = 0; k < data.length; k++ ) {
 
 		// check if participant belongs to current check country
-		if( country == data[k].country_code_3 ){
+		if ( country == data[k].countryCode3 ) {
 			
 			// count chosen options for participants per country
-			if( data[ k ].basic_income_vote == "I would vote for it"){
+			if ( data[k].basicIncomeVote == "I would vote for it" ) {
 				choice1 = choice1 + 1;
 			}
 			
-			else if( data[ k ].basic_income_vote == "I would probably vote for it"){
+			else if ( data[k].basicIncomeVote == "I would probably vote for it" ) {
 				choice2 = choice2 + 1;
 			}
 			
-			else if( data[ k ].basic_income_vote == "I would probably vote against it"){
+			else if ( data[k].basicIncomeVote == "I would probably vote against it" ) {
 				choice3 = choice3 + 1;
 			}
 			
-			else if( data[ k ].basic_income_vote == "I would vote against it"){
+			else if ( data[k].basicIncomeVote == "I would vote against it" ) {
 				choice4 = choice4 + 1;
 			}
 			
-			else if( data[ k ].basic_income_vote == "I would not vote"){
+			else if ( data[k].basicIncomeVote == "I would not vote" ) {
 				choice5 = choice5 + 1;
 			}
 			
-			else{
+			else {
 				other = other + 1;
 			}
 		totalParticipants = totalParticipants + 1;
 		}
 	}
-	console.log(totalParticipants);
-	return [ choice1, choice2, choice3, choice4, choice5, other, totalParticipants ];
+	
+	return [choice1, choice2, choice3, choice4, choice5, other, totalParticipants];
 }
 
 /* 
 showInfo function
 code for tab funtionality so it reacts on clicking and code is derived from: https://www.w3schools.com/howto/howto_js_tabs.asp
 */
-function showInfo(evt,show){
+function showInfo( evt,show ) {
 	// declare all variables
-	var i, tabContent, tabLinks;
+	var i;
+	var tabContent;
+	var tabLinks;
 
 	// get tabContent elements and hide them
-	tabContent = document.getElementsByClassName("tabContent");
-	for(i = 0; i < tabContent.length; i++){
+	tabContent = document.getElementsByClassName( "tabContent" );
+	for ( i = 0; i < tabContent.length; i++ ) {
 		tabContent[i].style.display = "none";
 	}
 
 	// show current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(show).style.display = "block";
+	document.getElementById( show ).style.display = "block";
 }
--- scripts/csvfileconverter.py
+++ scripts/csvfileconverter.py
@@ -8,36 +8,36 @@ import csv
 import json
 
 # open the input and output file
-input_file_1 = open("../basic_income_dataset_dalia.csv", "r")
-input_file_2 = open("../countryCodes.csv", "r")
-output_file = open("basicIncomeDoubleCountries.csv", 'w')
+inputFile1 = open( "../basic_income_dataset_dalia.csv", "r" )
+inputFile2 = open( "../countryCodes.csv", "r" )
+outputFile = open( "basicIncomeDoubleCountries.csv", "w" )
 
 # read write to the files
-reader_1 = csv.reader(input_file_1, delimiter = ",", quoting = csv.QUOTE_NONE)
-reader_2 = csv.reader(input_file_2, delimiter = ",", quoting = csv.QUOTE_NONE)
-writer = csv.writer(output_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+reader1 = csv.reader( inputFile1, delimiter = ",", quoting = csv.QUOTE_NONE )
+reader2 = csv.reader( inputFile2, delimiter = ",", quoting = csv.QUOTE_NONE )
+writer = csv.writer( outputFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL )
 
 # make column titles and write them to the output file
-fieldnames = ["country_code_2", "country_code_3", "age", "gender", "rural_urban", "education_level", "full_time", "children", "basic_income_awareness", "basic_income_vote", "basic_income_effect", "basic_income_arguments_for", "basic_income_arguments_against", "age_group", "weight"]
-writer.writerow(fieldnames)
+fieldNames = ["countryCode2", "countryCode3", "age", "gender", "ruralUrban", "educationLevel", "fullTime", "children", "basicIncomeAwareness", "basicIncomeVote", "basicIncomeEffect", "basicIncomeArgumentsFor", "basicIncomeArgumentsAgainst", "ageGroup", "weight"]
+writer.writerow( fieldNames )
 
 # claim variables that need to be an empty array or contain specific predefined values
 countries = []
-included_columns = [1, 2] 
+includedColumns = [1, 2] 
 
 # loop through county file and select all 2 and 3 letter country codes and put it in an array
-for row in reader_2:
-	country = list(row[i] for i in included_columns)
-	countries.append(country)
+for row in reader2:
+	country = list( row[i] for i in includedColumns )
+	countries.append( country )
 
 '''' 
 loop through the reader and put the data in correct format
 put them all together in an array and write them to the out-put file
 '''
-for row in reader_1:
-	for j in range(0, len(countries)):
+for row in reader1:
+	for j in range( 0, len( countries ) ):
 		if row[0] == countries[j][0]:
 			if row[0] != "":
 				out = [row[0], countries [j][1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10],  row[11],  row[12],  row[13],  row[14]]
-				writer.writerow(out)
+				writer.writerow( out )
 
